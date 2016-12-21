36e79d4bd0478a4a63a7e530776b832bd02187ce
Stephen <raphael.appsoluteperfection@gmail.com>
2016-12-20 23:02:57 -0600

    Removed command line arguments

--- config/settings.js
+++ config/settings.js
@@ -1,27 +1,15 @@
 (function(){
     'use strict';
 
-    const commandLineArgs = require('command-line-args');
-    const optionDefinitions = [
-        { name: 'username', alias: 'u', type: String, defaultValue: '' },
-        { name: 'password', alias: 'p', type: String, defaultValue: '' },
-        { name: 'browser', alias: 'b', type: String, defaultValue: '' },
-    ];
-    const options = commandLineArgs(optionDefinitions);
-
     let settings = {};
-
-    settings.username = options.username;
-    settings.password = options.password;
-    settings.browser = options.browser;
     
-    if (!settings.username && process.env.IPFOB_USERNAME){
+    if (process.env.IPFOB_USERNAME){
         settings.username = process.env.IPFOB_USERNAME; 
     }
-    if (!settings.password && process.env.IPFOB_PASSWORD){
+    if (process.env.IPFOB_PASSWORD){
         settings.password = process.env.IPFOB_PASSWORD; 
     }
-    if (!settings.browser && process.env.SELENIUM_BROWSER){
+    if (process.env.SELENIUM_BROWSER){
         settings.browser = process.env.SELENIUM_BROWSER; 
     }
 
